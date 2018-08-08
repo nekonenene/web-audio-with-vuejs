5274e1652ed5459c3fa443826fb0ceffdd27d94b
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2018-08-09 05:06:25 +0900

    ボタンで再生・停止されるように

--- src/js/top.js
+++ src/js/top.js
@@ -6,6 +6,17 @@ window.onload = () => {
   console.log('hoge');
 };
 
+const getAudioContext = () => {
+  try {
+    // Fix up for prefixing
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    return new AudioContext();
+  } catch (e) {
+    alert('Sorry, Web Audio API is not supported in this browser');
+    return null;
+  }
+};
+
 const vm = new Vue({
   el: '#app',
   components: {
@@ -18,10 +29,24 @@ const vm = new Vue({
       height: 16,
       dotSize: 18,
     },
+    audioContext: null,
+    oscNode: null,
+    playBtnTxt: "Play",
+  },
+  created: function () {
+    this.audioContext = getAudioContext();
   },
   watch: {
     value1: function (val) {
       // console.log(val);
+      this.playOSC(440.0);
+    },
+    oscNode: function () {
+      if (this.oscNode == null) {
+        this.playBtnTxt = "Play";
+      } else {
+        this.playBtnTxt = "Stop";
+      }
     },
   },
   computed: {
@@ -31,5 +56,29 @@ const vm = new Vue({
     },
   },
   methods: {
+    playOSC: function (freq = 440.0, type = "sine") {
+      if (this.oscNode != null) {
+        this.oscNode.stop();
+        this.oscNode = null;
+        return;
+      }
+
+      this.oscNode = this.audioContext.createOscillator();
+
+      /**
+       * 波形:
+       *  "sine" : サイン波
+       *  "square" : 矩形波
+       *  "sawtooth":鋸歯状波
+       *  "triangle":三角波
+       */
+      this.oscNode.type = type;
+      // オシレーターの周波数を決定
+      this.oscNode.frequency.value = freq;
+      // オシレーターを最終出力に接続
+      this.oscNode.connect(this.audioContext.destination);
+      // オシレーター動作
+      this.oscNode.start();
+    },
   },
 });
